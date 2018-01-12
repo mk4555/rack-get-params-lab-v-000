399f57a6383f3bee827821f447f42595cded372f
Min Kim <mk4555@nyu.edu>
2018-01-12 00:30:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,10 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      search_term = req.params["q"]
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
